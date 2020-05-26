c721d5a7df6c89567021bc6e8cb7ec0de655c177
Simón Holmquist <simon.holmquist@gmail.com>
2020-05-26 18:35:11 -0300

    solucion invertir numero

--- tpn6/Program.cs
+++ tpn6/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace tpn6
 {
@@ -6,7 +7,25 @@ namespace tpn6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string entrada;
+            do
+            {
+                Console.WriteLine("Ingrese el número que desea invertir");
+                entrada = Console.ReadLine();
+
+            } while (Convert.ToInt32(entrada)<=0);
+            Console.WriteLine("El número invertido es: " + Invertir(entrada));
         }
+
+        public static string Invertir(string entrada)
+        {
+            string salida = "";
+            for (int i = entrada.Length - 1; i >= 0; i--)
+            {
+                salida += entrada[i];
+            }
+            return salida;
+        }
+
     }
 }
