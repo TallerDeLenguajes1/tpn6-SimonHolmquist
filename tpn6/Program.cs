eb161f70ea39f0522cc89b250f93b2fb38e90f21
Simón Holmquist <simon.holmquist@gmail.com>
2020-05-26 19:47:02 -0300

    Ejercicio 3

--- tpn6/Program.cs
+++ tpn6/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using static System.Console;
 using System.ComponentModel.DataAnnotations;
+using static System.Math;
 
 namespace tpn6
 {
@@ -7,31 +9,58 @@ namespace tpn6
     {
         static void Main(string[] args)
         {
+            //Ejercicio1();
+            //Ejercicio2();
+            //Ejercicio3();
+        }
+
+        public static string Invertir(string entrada)
+        {
+            string salida = "";
+            for (int i = entrada.Length - 1; i >= 0; i--)
+            {
+                salida += entrada[i];
+            }
+            return salida;
+        }
+
+        public static void ImprimirTítutlo(string titulo)
+        {
+            WriteLine("".PadLeft(titulo.Length + 4, '*'));
+            WriteLine($"* {titulo} *");
+            WriteLine("".PadLeft(titulo.Length + 4, '*'));
+        }
+
+        public static void Ejercicio1()
+        {
             ImprimirTítutlo("Ejercicio 1");
             string entrada;
-            int opcion, a, b, resultado = 0;
             do
             {
-                Console.WriteLine("Ingrese el número que desea invertir");
-                entrada = Console.ReadLine();
+                WriteLine("Ingrese el número que desea invertir");
+                entrada = ReadLine();
 
-            } while (Convert.ToInt32(entrada)<=0);
-            Console.WriteLine("El número invertido es: " + Invertir(entrada));
+            } while (Convert.ToInt32(entrada) <= 0);
+            WriteLine("El número invertido es: " + Invertir(entrada));
+        }
+        public static void Ejercicio2()
+        {
+            int opcion, a, b, resultado = 0;
             ImprimirTítutlo("Ejercicio 2");
             do
             {
                 opcion = 0;
                 do
                 {
-                    Console.WriteLine("Ingrese la operación que desea realizar:\n1.Sumar\n2.Restar\n3.Multiplicar\n4.Dividir");
-                    opcion = Convert.ToInt32(Console.ReadLine());
-                    if(opcion != 1 && opcion != 2 && opcion != 3 && opcion != 4)
-                        Console.WriteLine("La opción ingresada es incorrecta. Ingrese nuevamente.");
+                    WriteLine("Ingrese la operación que desea realizar:\n1.Sumar\n2.Restar\n3.Multiplicar\n4.Dividir");
+                    opcion = Convert.ToInt32(ReadLine());
+                    if (opcion != 1 && opcion != 2 && opcion != 3 && opcion != 4)
+                        WriteLine("La opción ingresada es incorrecta. Ingrese nuevamente.");
                 } while (opcion != 1 && opcion != 2 && opcion != 3 && opcion != 4);
-                Console.WriteLine("Ingrese primer valor");
-                a = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Ingrese segundo valor");
-                b = Convert.ToInt32(Console.ReadLine());
+                WriteLine("Ingrese primer valor");
+                a = Convert.ToInt32(ReadLine());
+                WriteLine("Ingrese segundo valor");
+                b = Convert.ToInt32(ReadLine());
                 switch (opcion)
                 {
                     case 1:
@@ -47,35 +76,46 @@ namespace tpn6
                         if (b != 0)
                             resultado = a / b;
                         else
-                            Console.WriteLine("ERROR: No se puede dividir por 0");
+                            WriteLine("ERROR: No se puede dividir por 0");
                         break;
                     default:
                         resultado = 0;
                         break;
                 }
-                Console.WriteLine("El resultado de la operación es: " + resultado);
-                Console.WriteLine("¿Desea realizar otra operación?\n1.Si\n2.No");
-                opcion = Convert.ToInt32(Console.ReadLine());
+                WriteLine("El resultado de la operación es: " + resultado);
+                WriteLine("¿Desea realizar otra operación?\n1.Si\n2.No");
+                opcion = Convert.ToInt32(ReadLine());
                 if (opcion == 2)
                     opcion = 0;
             } while (opcion != 0);
         }
-
-        public static string Invertir(string entrada)
+        public static void Ejercicio3()
         {
-            string salida = "";
-            for (int i = entrada.Length - 1; i >= 0; i--)
-            {
-                salida += entrada[i];
-            }
-            return salida;
-        }
-
-        public static void ImprimirTítutlo(string titulo)
-        {
-            Console.WriteLine("".PadLeft(titulo.Length + 4, '*'));
-            Console.WriteLine($"* {titulo} *");
-            Console.WriteLine("".PadLeft(titulo.Length + 4, '*'));
+            ImprimirTítutlo("Ejercicio 3");
+            WriteLine("Ingrese un número");
+            int num = Convert.ToInt32(ReadLine());
+            //El valor absoluto de un número
+            //● El cuadrado
+            //● La raíz cuadrada
+            //● El seno
+            //● El Coseno
+            //● La parte entera de un tipo float.
+            //Luego de esto, solicite dos números al usuario y determine:
+            //● El Máximo entre los dos números
+            //● El Mínimo entre los dos números
+            WriteLine($"El cuadrado de {num} es {Math.Pow(num, 2)}");
+            WriteLine($"La raiz cuadrada de {num} es {Math.Sqrt(num)}");
+            WriteLine($"El seno de {num} es {Math.Sin(num)}");
+            WriteLine($"El coseno de {num} es {Math.Cos(num)}");
+            WriteLine("Ingrese un número con , flotante");
+            double numF = Convert.ToDouble(ReadLine());
+            WriteLine($"La parte entera de {numF} es {Math.Floor(numF)}");
+            WriteLine("Intrese un número");
+            int num1 = Convert.ToInt32(ReadLine());
+            WriteLine("Intrese otro número");
+            int num2 = Convert.ToInt32(ReadLine());
+            WriteLine($"El máximo entre {num1} y {num2} es: {Math.Max(num1, num2)}");
+            WriteLine($"El mínimo entre {num1} y {num2} es: {Math.Min(num1, num2)}");
         }
     }
 }
